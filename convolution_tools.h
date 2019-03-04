4a9732c8da71142a270973d3f2859f973b041862
SurielAsh <lihaz@qq.com>
2019-03-04 21:50:25 +0800

    v.0.8.190304 fix bugs normalized naming

---
+++ convolution_tools.h
@@ -0,0 +1,235 @@
+//
+// Created by nagi on 19-3-1.
+//
+
+#ifndef CNNTRY_STRUCT_TOOLS_H
+#define CNNTRY_STRUCT_TOOLS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "toolbox.h"
+using std::vector;
+
+struct convolution{
+	int d;
+	convolution()= default;
+	vector<vector<int>> self;
+	vector<int> fulcnet;
+	void ini();
+	void ex(int ex);
+	void max_pooling(int step,int dim);
+	void arg_pooling(int step,int dim);
+	void relu();
+	void fulc();
+	int size()
+	{
+		return self.size();
+	}
+};
+
+void convolution::ini()
+{
+	vini(self,d);
+}
+
+void convolution::ex(int ex)
+{
+	vector<vector<int>> tmp;
+	vini(tmp,d+ex);
+	for(int i=0;i<d;i++)
+	{
+		for(int k=0;k<d;k++)
+			tmp[i][k]=self[i][k];
+	}
+	self=tmp;
+}
+
+void convolution::max_pooling(int step,int dim)
+{
+	if((d-dim)%step!=0)
+		ex((d-dim)%step);
+	int tim=(d-dim)/step+1;
+	convolution tmp;
+	tmp.d=tim;
+	tmp.ini();
+	for(int ix=0,kx=0;ix<=d-dim;ix+=step,kx++)
+	{
+		for(int iy=0,ky=0;iy<=d-dim;iy+=step,ky++)
+		{
+			int max = 0;
+			for (int x = 0; x < dim; x++)
+			{
+				for (int y = 0; y < dim; y++)
+				{
+					if (self[ix + x][iy + y] > max)
+						max = self[ix + x][iy + y];
+				}
+			}
+			tmp.self[kx][ky]=max;
+
+		}
+	}
+	self=tmp.self;
+	d=tmp.d;
+}
+
+void convolution::arg_pooling(int step,int dim)
+{
+	if((d-dim)%step!=0)
+		ex((d-dim)%step);
+	int tim=(d-dim)/step+1;
+	convolution tmp;
+	tmp.d=tim;
+	tmp.ini();
+	for(int ix=0,kx=0;ix<=d-dim;ix+=step,kx++)
+	{
+		for(int iy=0,ky=0;iy<=d-dim;iy+=step,ky++)
+		{
+			int sum = 0;
+			for (int x = 0; x < dim; x++)
+			{
+				for (int y = 0; y < dim; y++)
+				{
+					sum += self[ix + x][iy + y];
+				}
+			}
+			tmp.self[kx][ky]=sum/(dim^2);
+
+		}
+	}
+	self=tmp.self;
+	d=tmp.d;
+}
+
+void convolution::relu()
+{
+	for(auto &a:self)
+	{
+		for(auto &k:a) {
+			if (k < 0)
+				k = 0;
+		}
+	}
+}
+
+void convolution::fulc()
+{
+	for(auto &a:self)
+	{
+		for(auto &k:a) {
+			fulcnet.push_back(k);
+		}
+	}
+}
+
+struct convlcore{
+	convlcore ()= default;
+	vector<vector<int>> self;
+	int d,bias;
+	void ini();
+};
+
+void convlcore::ini()
+{
+	InitRand();
+	vini(self,d);
+	for(auto &a:self)
+	{
+		for(auto &k:a) {
+			k=rand()%255;
+		}
+	}
+}
+
+convolution max_pooling(convolution tar,int pstep,int pdim)
+{
+	if((tar.d-pdim)%pstep!=0)
+		tar.ex((tar.d-pdim)%pstep);
+	int tim=(tar.d-pdim)/pstep+1;
+	convolution tmp;
+	tmp.d=tim;
+	tmp.ini();
+	for(int ix=0,kx=0;ix<=tar.d-pdim;ix+=pstep,kx++)
+	{
+		for(int iy=0,ky=0;iy<=tar.d-pdim;iy+=pstep,ky++)
+		{
+			int max = 0;
+			for (int x = 0; x < pdim; x++)
+			{
+				for (int y = 0; y < pdim; y++)
+				{
+					if (tar.self[ix + x][iy + y] > max)
+						max = tar.self[ix + x][iy + y];
+				}
+			}
+			tmp.self[kx][ky]=max;
+
+		}
+	}
+	return tmp;
+}
+
+convolution arg_pooling(convolution tar,int step,int dim)
+{
+	if((tar.d-dim)%step!=0)
+		tar.ex((tar.d-dim)%step);
+	int tim=(tar.d-dim)/step+1;
+	convolution tmp;
+	tmp.d=tim;
+	tmp.ini();
+	for(int ix=0,kx=0;ix<=tar.d-dim;ix+=step,kx++)
+	{
+		for(int iy=0,ky=0;iy<=tar.d-dim;iy+=step,ky++)
+		{
+			int sum = 0;
+			for (int x = 0; x < dim; x++)
+			{
+				for (int y = 0; y < dim; y++)
+				{
+					sum += tar.self[ix + x][iy + y];
+				}
+			}
+			tmp.self[kx][ky]=sum/(dim^2);
+
+		}
+	}
+	return tmp;
+}
+
+convolution single_convoluting(convolution target, convlcore core, int step)
+{
+	if((target.d-core.d)%step!=0)
+		target.ex((target.d-core.d)%step);
+	int tim=(target.d-core.d)/step+1;
+	convolution result;
+	vini(result.self,tim);
+	for(int ix=0,kx=0;ix<=target.d-core.d;ix+=step,kx++)
+	{
+		for(int iy=0,ky=0;iy<=target.d-core.d;iy+=step,ky++)
+		{
+			int sum=0;
+			for (int x = 0; x < core.d; x++)
+			{
+				for (int y = 0; y < core.d; y++)
+				{
+					sum += (target.self[ix + x][iy + y]*core.self[x][y]+core.bias);
+				}
+			}
+			result.self[kx][ky]=sum;
+		}
+	}
+	return result;
+}
+
+vector<convolution> convoluting(convolution target,vector<convlcore> corelist,int step)
+{
+	vector<convolution> result;
+	for(int i=0;i<corelist.size();i++)
+	{
+		result.push_back(single_convoluting(target,corelist[i],step));
+	}
+	return result;
+}
+
+#endif //CNNTRY_STRUCT_TOOLS_H
--- main.cpp
+++ main.cpp
@@ -2,51 +2,13 @@
 #include <string>
 #include <vector>
 #include "toolbox.h"
-#include "consolve_tools.h"
+#include "convolution_tools.h"
+#include "image_processing.h"
 
 using namespace std;
 
 int main()
 {
-	int dim,cstep,core_dim,pstep,pooling_dim;
-	dim=64;
-	cstep=2;
-	core_dim=3;
-	pstep=2;
-	pooling_dim=3;
-	conslv ori,ori_1,ori_2;
-	cslcore core_1,core_2,core_3,core_4;
-	ori.d=dim;
-	ori_1.d=dim;
-	ori_2.d=dim;
-	ori.ini();
-	ori_1.ini();
-	ori_2.ini();
-	core_1.d=core_dim+2;
-	core_2.d=core_dim+2;
-	core_3.d=core_dim;
-	core_4.d=core_dim;
-	core_1.pow=0;
-	core_2.pow=1;
-	core_3.pow=0;
-	core_4.pow=0;
-	core_1.ini();
-	core_2.ini();
-	core_3.ini();
-	core_4.ini();
-	ori_1=consolve(ori,core_1,cstep);
-	ori_2=consolve(ori,core_2,cstep);
-	ori_1.disnega_relu();
-	ori_2.disnega_relu();
-	ori_1.max_pooling(pstep,pooling_dim);
-	ori_2.max_pooling(pstep,pooling_dim);
-	ori_1=consolve(ori_1,core_3,cstep);
-	ori_2=consolve(ori_2,core_4,cstep);
-	ori_1.disnega_relu();
-	ori_2.disnega_relu();
-	ori_1.max_pooling(pstep,pooling_dim);
-	ori_2.max_pooling(pstep,pooling_dim);
-	ori_1.fulic();
-	ori_2.fulic();
+
 	return 0;
 }
